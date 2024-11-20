56dfae47a2fc56b025332cb43caa2b37fabcbae1
Duy2763 <tduy2706@gmail.com>
2024-11-21 03:14:01 +0700

    Update api

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000; // Sử dụng biến môi trường PORT
+const port = process.env.PORT || 3001; // Sử dụng biến môi trường PORT
 
 const songRoutes = require('./routes/songRoutes');
 const artistRoutes = require('./routes/artistRoutes');
@@ -28,7 +28,7 @@ app.use('/assets/images/artist', express.static(path.join(__dirname, 'assets/ima
 app.use('/assets/images/banner', express.static(path.join(__dirname, 'assets/images/banner')));
 app.use('/assets/images/song', express.static(path.join(__dirname, 'assets/images/song')));
 app.use('/assets/images/chart', express.static(path.join(__dirname, 'assets/images/chart')));
-app.use('/assets/images/chart', express.static(path.join(__dirname, 'assets/audios')));
+app.use('/assets/audios', express.static(path.join(__dirname, 'assets/audios')));
 
 // Kết nối MongoDB
 mongoose.connect(process.env.MONGODB_URI, { // Sử dụng biến môi trường MONGODB_URI
